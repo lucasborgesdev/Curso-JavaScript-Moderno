439ab182a0fe7980ce7dd993e77de111dd16bb04
lucasborgesdev <lucas_borges_br@hotmail.com>
2020-04-12 00:58:26 -0300

    finalizado for-In

---
+++ forIn/index.js
@@ -0,0 +1,16 @@
+
+const fruta = ['banana', 'melão', 'maça'];
+
+
+const pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Borges',
+    idade: 30
+};
+
+console.log(pessoa.nome);
+console.log(pessoa['nome']);
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+}
